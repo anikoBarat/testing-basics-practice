1b9e85555860053ac0d6d723e82d60225370e6ca
anikoBarat <aniko.barat@gmail.com>
2018-01-21 15:09:53 +0100

    Classes for practice basic testing concepts is ready

---
+++ com.codecool.calculator/src/BasicCalculator.java
@@ -0,0 +1,28 @@
+public class BasicCalculator implements Calculator {
+
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+
+    @Override
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+
+    @Override
+    public float divide(int a, int b) throws IllegalArgumentException {
+        if(b == 0){
+            throw new IllegalArgumentException("Division with 0 isn't a valid operation!");
+        }
+        return a / b;
+    }
+
+
+    @Override
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ com.codecool.calculator/src/Calculator.java
@@ -0,0 +1,11 @@
+public interface Calculator {
+
+    int add(int a, int b);
+
+    int subtract(int a, int b);
+
+    float divide(int a, int b);
+
+    int multiply(int a, int b);
+
+}
